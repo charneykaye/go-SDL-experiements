590cfe3a04b5110a9c06b0ec376bb5f29883ad84
Charney Kaye <charney@outright.io>
2016-01-04 01:17:29 -0500

    update implementation of WIP atomix

--- mixing/mixing.go
+++ mixing/mixing.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	sampleHz   = 44100
-	numSamples = 4096
+	numSamples = 22050
 )
 
 func main() {
@@ -25,10 +25,18 @@ func main() {
 		}).Fatal("Cannot init SDL")
 		return
 	}
-	defer sdl.Quit() // TODO: wrap this with func: recover from panic (pause audio stream?) then sdl.Quit
+    defer func() {
+        if r := recover(); r != nil {
+			log.WithFields(log.Fields{
+				"recover": r,
+			}).Warn("Player Recovered")
+        }
+		sdl.PauseAudio(true)
+		atomix.Teardown()
+		sdl.Quit()
+    }()
 
 	var (
-		start = time.Now().Add(1 * time.Second) // 1 second delay before start
 		step = 125 * time.Millisecond
 		loops = 4
 	)
@@ -41,36 +49,34 @@ func main() {
 		marac = p808 + "maracas.wav"
 	)
 
-	spec := atomix.Spec(sdl.AudioSpec{
-		Freq:     sampleHz,
-		Format:   sdl.AUDIO_U16,
-		Channels: 2,
-		Samples:  numSamples,
-	})
+	atomix.Debug(true)
+	atomix.Configure(sdl.AudioSpec{
+			Freq:     sampleHz,
+			Format:   sdl.AUDIO_U16,
+			Channels: 2,
+			Samples:  numSamples,
+		})
 
-	t := start
-	loopDur := 16 * step
-	totalDur := time.Duration(0)
+	t := 1 * time.Second // padding before music
 	for n := 0; n < loops; n++ {
-		atomix.Play(kick1, t, 1)
-		atomix.Play(marac, t.Add(1 * step), 0.5)
-		atomix.Play(snare, t.Add(4 * step), 0.8)
-		atomix.Play(marac, t.Add(6 * step), 0.5)
-		atomix.Play(kick2, t.Add(7 * step), 0.9)
-		atomix.Play(marac, t.Add(10 * step), 0.5)
-		atomix.Play(kick2, t.Add(10 * step), 0.9)
-		atomix.Play(snare, t.Add(12 * step), 0.8)
-		atomix.Play(marac, t.Add(14 * step), 0.5)
-		t = t.Add(loopDur)
-		totalDur += loopDur
+        atomix.Play(kick1, t,            4 *step,  1.0)
+        atomix.Play(marac, t + 1 *step,  1 *step,  0.5)
+        atomix.Play(snare, t + 4 *step,  4 *step,  0.8)
+        atomix.Play(marac, t + 6 *step,  1 *step,  0.5)
+        atomix.Play(kick2, t + 7 *step,  4 *step,  0.9)
+        atomix.Play(marac, t + 10 *step, 1 *step,  0.5)
+        atomix.Play(kick2, t + 10 *step, 4 *step,  0.9)
+        atomix.Play(snare, t + 12 *step, 4 *step,  0.8)
+        atomix.Play(marac, t + 14 *step, 1 *step,  0.5)
+		t += 16 * step
 	}
-	runLength := totalDur + 2 * time.Second
 
+	spec := atomix.Spec()
 	sdl.OpenAudio(spec, nil)
 	sdl.PauseAudio(false)
 	log.WithFields(log.Fields{
 		"spec": spec,
-	}).Info("")
+	}).Info("SDL OpenAudio > Atomix")
 
-	time.Sleep(runLength)
+	time.Sleep(t + 1 * time.Second) // padding after music
 }
